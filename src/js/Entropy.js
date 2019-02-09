9f7f9221d1b1fe201e01558258239dbf65773698
Silas <me@silentsilas.com>
2019-02-09 14:09:58 -0500

    convert to editorconfig settings

--- src/js/Entropy.js
+++ src/js/Entropy.js
@@ -2,148 +2,148 @@ import Settings from './Settings.js';
 
 export default class {
 
-    static createComponent({richActor, combinations}) {
-        const container = document.createElement("div");
-        container.classList.add('crack_container');
+  static createComponent({richActor, combinations}) {
+    const container = document.createElement("div");
+    container.classList.add('crack_container');
 
-        let guessesPerSecond;
-        switch (richActor) {
-            case 'guy':
-                richActor = 'that one sketchy dude in a black hoodie at your local coffee shop'
-                guessesPerSecond = 100000000000;
-                break;
-            case 'company':
-                richActor = 'Evil Corp'
-                guessesPerSecond = 1000000000000;
-                break;
-            case 'state':
-                richActor = 'Big Brother'
-                guessesPerSecond = 100000000000000;
-                break;
-            case 'universe':
-                richActor = 'the Illuminati';
-                guessesPerSecond = 1000000000000000;
-                break;
-            default:
-                throw new Error("Invalid actor.");
-        }
+    let guessesPerSecond;
+    switch (richActor) {
+      case 'guy':
+        richActor = 'that one sketchy dude in a black hoodie at your local coffee shop'
+        guessesPerSecond = 100000000000;
+        break;
+      case 'company':
+        richActor = 'Evil Corp'
+        guessesPerSecond = 1000000000000;
+        break;
+      case 'state':
+        richActor = 'Big Brother'
+        guessesPerSecond = 100000000000000;
+        break;
+      case 'universe':
+        richActor = 'the Illuminati';
+        guessesPerSecond = 1000000000000000;
+        break;
+      default:
+        throw new Error("Invalid actor.");
+      }
 
-        // total time to go through all combinations
-        let guarantee = (combinations / guessesPerSecond);
+      // total time to go through all combinations
+      let guarantee = (combinations / guessesPerSecond);
 
-        let timescale = 'seconds';
-        // break it down into desired scale of time
-        if (guarantee > 60) {
-            timescale = 'minutes';
-            guarantee = guarantee / 60;
-        }
-        if (guarantee > 60) {
-            timescale = 'hours';
-            guarantee = guarantee / 60;
-        }
-        if (guarantee > 24) {
-            timescale = 'days';
-            guarantee = guarantee / 24;
-        }
-        if (guarantee > 365) {
-            timescale = 'years';
-            guarantee = guarantee / 365;
-        }
-        if (guarantee > 1000) {
-            timescale = 'millennia';
-            guarantee = guarantee / 1000;
-        }
-        if (guarantee > 1000) {
-            timescale = 'million years';
-            guarantee = guarantee / 1000;
-        }
-        if (guarantee > 1000) {
-            timescale = 'billion years';
-            guarantee = guarantee / 1000;
-        }
-        if (guarantee > 1000) {
-            timescale = 'trillion years';
-            guarantee = guarantee / 1000;
-        }
-        
+      let timescale = 'seconds';
+      // break it down into desired scale of time
+      if (guarantee > 60) {
+        timescale = 'minutes';
+        guarantee = guarantee / 60;
+      }
+      if (guarantee > 60) {
+        timescale = 'hours';
+        guarantee = guarantee / 60;
+      }
+      if (guarantee > 24) {
+        timescale = 'days';
+        guarantee = guarantee / 24;
+      }
+      if (guarantee > 365) {
+        timescale = 'years';
+        guarantee = guarantee / 365;
+      }
+      if (guarantee > 1000) {
+        timescale = 'millennia';
+        guarantee = guarantee / 1000;
+      }
+      if (guarantee > 1000) {
+        timescale = 'million years';
+        guarantee = guarantee / 1000;
+      }
+      if (guarantee > 1000) {
+        timescale = 'billion years';
+        guarantee = guarantee / 1000;
+      }
+      if (guarantee > 1000) {
+        timescale = 'trillion years';
+        guarantee = guarantee / 1000;
+      }
+      
 
-        // now actually create the HTML component
-        const header = document.createElement("div");
-        header.classList.add('crack_header');
-        header.appendChild(document.createTextNode(
-            `For ${richActor} to crack it:`
-        ));
-        container.appendChild(header);
+      // now actually create the HTML component
+      const header = document.createElement("div");
+      header.classList.add('crack_header');
+      header.appendChild(document.createTextNode(
+        `For ${richActor} to crack it:`
+      ));
+      container.appendChild(header);
 
-        const contentDiv = document.createElement("div");
-        contentDiv.classList.add('crack_content');
+      const contentDiv = document.createElement("div");
+      contentDiv.classList.add('crack_content');
 
-        // the guaranteed text
-        const guaranteeHeader = document.createElement("h3");
-        guaranteeHeader.appendChild(document.createTextNode(
-            'Guaranteed:'
-        ));
-        contentDiv.appendChild(guaranteeHeader);
-        contentDiv.appendChild(document.createTextNode(
-            `${guarantee.toLocaleString()} ${timescale}`
-        ));
+      // the guaranteed text
+      const guaranteeHeader = document.createElement("h3");
+      guaranteeHeader.appendChild(document.createTextNode(
+        'Guaranteed:'
+      ));
+      contentDiv.appendChild(guaranteeHeader);
+      contentDiv.appendChild(document.createTextNode(
+        `${guarantee.toLocaleString()} ${timescale}`
+      ));
 
-        // TODO: maybe turn guarantee + average into separate divs?
-        contentDiv.appendChild(document.createElement('br'));
+      // TODO: maybe turn guarantee + average into separate divs?
+      contentDiv.appendChild(document.createElement('br'));
 
-        // the average text
-        const averageHeader = document.createElement("h3");
-        averageHeader.appendChild(document.createTextNode(
-            'Average:'
-        ));
-        contentDiv.appendChild(averageHeader);
-        contentDiv.appendChild(document.createTextNode(
-            `${(guarantee/2).toLocaleString()} ${timescale}`
-        ));
+      // the average text
+      const averageHeader = document.createElement("h3");
+      averageHeader.appendChild(document.createTextNode(
+        'Average:'
+      ));
+      contentDiv.appendChild(averageHeader);
+      contentDiv.appendChild(document.createTextNode(
+        `${(guarantee/2).toLocaleString()} ${timescale}`
+      ));
 
-        container.appendChild(contentDiv);
+      container.appendChild(contentDiv);
 
-        return container;
-    }
+      return container;
+  }
 
-    static calculateEntropy(div) {
-        let range = 0;
-        range += Settings.uppercase.allowed ? Settings.uppercase.COUNT : 0;
-        range += Settings.lowercase.allowed ? Settings.lowercase.COUNT : 0;
-        range += Settings.digits.allowed ? Settings.digits.COUNT : 0;
-        const entropyPerCharacter = Math.log2(range);
-        const combinations = Math.pow(range, Settings.password_length);
-        const totalEntropy = entropyPerCharacter * Settings.password_length;
+  static calculateEntropy(div) {
+    let range = 0;
+    range += Settings.uppercase.allowed ? Settings.uppercase.COUNT : 0;
+    range += Settings.lowercase.allowed ? Settings.lowercase.COUNT : 0;
+    range += Settings.digits.allowed ? Settings.digits.COUNT : 0;
+    const entropyPerCharacter = Math.log2(range);
+    const combinations = Math.pow(range, Settings.password_length);
+    const totalEntropy = entropyPerCharacter * Settings.password_length;
 
-        const entropyDiv = document.createElement('div');
-        entropyDiv.classList.add('entropy');
+    const entropyDiv = document.createElement('div');
+    entropyDiv.classList.add('entropy');
 
-        entropyDiv.appendChild(document.createTextNode(`Unique Characters: ${range}`));
-        entropyDiv.appendChild(document.createElement('br'));
-        entropyDiv.appendChild(document.createTextNode(`Total Password Combinations: ${combinations.toLocaleString()}`));
-        entropyDiv.appendChild(document.createElement('br'));
-        entropyDiv.appendChild(document.createTextNode(`Entropy: ${totalEntropy}`));
-        entropyDiv.appendChild(document.createElement('br'));
-        div.appendChild(entropyDiv);
+    entropyDiv.appendChild(document.createTextNode(`Unique Characters: ${range}`));
+    entropyDiv.appendChild(document.createElement('br'));
+    entropyDiv.appendChild(document.createTextNode(`Total Password Combinations: ${combinations.toLocaleString()}`));
+    entropyDiv.appendChild(document.createElement('br'));
+    entropyDiv.appendChild(document.createTextNode(`Entropy: ${totalEntropy}`));
+    entropyDiv.appendChild(document.createElement('br'));
+    div.appendChild(entropyDiv);
 
-        div.appendChild(this.createComponent({
-            richActor: 'guy',
-            combinations: combinations
-        }));
-        div.appendChild(this.createComponent({
-            richActor: 'company',
-            combinations: combinations
-        }));
-        div.appendChild(this.createComponent({
-            richActor: 'state',
-            combinations: combinations
-        }));
-        div.appendChild(this.createComponent({
-            richActor: 'universe',
-            combinations: combinations
-        }));
+    div.appendChild(this.createComponent({
+      richActor: 'guy',
+      combinations: combinations
+    }));
+    div.appendChild(this.createComponent({
+      richActor: 'company',
+      combinations: combinations
+    }));
+    div.appendChild(this.createComponent({
+      richActor: 'state',
+      combinations: combinations
+    }));
+    div.appendChild(this.createComponent({
+      richActor: 'universe',
+      combinations: combinations
+    }));
 
-        div.innerHTML += '<br>Going by this chart: <a href="http://i.imgur.com/e3mGIFY.png">http://i.imgur.com/e3mGIFY.png</a><br><br>';
-        console.log(Math.log2(100000) * 4);
-    }
+    div.innerHTML += '<br>Going by this chart: <a href="http://i.imgur.com/e3mGIFY.png">http://i.imgur.com/e3mGIFY.png</a><br><br>';
+    console.log(Math.log2(100000) * 4);
+  }
 }
--- src/js/Main.js
+++ src/js/Main.js
@@ -3,66 +3,64 @@ import Entropy from './Entropy.js';
 import Words from './Words.js';
 
 export default class {
-    constructor({output, uppercaseEl, lowercaseEl, digitsEl, lengthEl, lengthElOutput}) {
-        this.output = output;
-        this.uppercaseEl = uppercaseEl;
-        this.lowercaseEl = lowercaseEl;
-        this.digitsEl = digitsEl;
-        this.lengthEl = lengthEl;
-        this.lengthElOutput = lengthElOutput;
+  constructor({output, uppercaseEl, lowercaseEl, digitsEl, lengthEl, lengthElOutput}) {
+    this.output = output;
+    this.uppercaseEl = uppercaseEl;
+    this.lowercaseEl = lowercaseEl;
+    this.digitsEl = digitsEl;
+    this.lengthEl = lengthEl;
+    this.lengthElOutput = lengthElOutput;
 
-        this.lengthEl.addEventListener('input', () => {
-            this.lengthElOutput.innerHTML = this.lengthEl.value;
-        });
-        this.words = new Words();
+    this.lengthEl.addEventListener('input', () => {
+      this.lengthElOutput.innerHTML = this.lengthEl.value;
+    });
+    this.words = new Words();
+  }
+
+  // we'll check the state of our checkboxes and update our Settings obj accordingly
+  updateSettings() {
+    Settings.uppercase.allowed = this.uppercaseEl.checked;
+    Settings.lowercase.allowed = this.lowercaseEl.checked;
+    Settings.digits.allowed = this.digitsEl.checked;
+    Settings.password_length = this.lengthEl.value;
+  }
+
+  getAllowedChars() {
+    // build up our array of allowed Unicode values
+    let allowed = [];
+    allowed = Settings.uppercase.allowed ? allowed.concat(Settings.uppercase.UNICODES) : allowed;
+    allowed = Settings.lowercase.allowed ? allowed.concat(Settings.lowercase.UNICODES) : allowed;
+    allowed = Settings.digits.allowed ? allowed.concat(Settings.digits.UNICODES) : allowed;
+
+    // if no checkboxes were checked, then we have no unicode values to choose from
+    if (allowed.length <= 0) {
+      let errorMsg = "Must enable at least one type of character.";
+      this.output.innerHTML = errorMsg;
+      throw new Error(errorMsg);
     }
+    return allowed;
+  }
 
-    // we'll check the state of our checkboxes and update our Settings obj accordingly
-    updateSettings() {
-        Settings.uppercase.allowed = this.uppercaseEl.checked;
-        Settings.lowercase.allowed = this.lowercaseEl.checked;
-        Settings.digits.allowed = this.digitsEl.checked;
-        Settings.password_length = this.lengthEl.value;
+  generatePassword() {
+    this.output.innerHTML = '';
+    this.updateSettings();
+    
+    // get our array of valid unicode values that we can choose from
+    const allowedChars = this.getAllowedChars();
+
+    // create an array of cryptographically-random values
+    const random = new Uint8Array(Settings.password_length);
+    window.crypto.getRandomValues(random);
+
+    // now we create the password string
+    let password = '';
+    for (let idx = 0; idx < Settings.password_length; idx++) {
+      // use the next random value to choose a unicode character
+      const validIdx = random[idx] % allowedChars.length
+      password += String.fromCharCode(allowedChars[validIdx]);
     }
+    this.output.innerHTML += `${password}<br><br>`;
 
-    getAllowedChars() {
-        // build up our array of allowed Unicode values
-        let allowed = [];
-        allowed = Settings.uppercase.allowed ? allowed.concat(Settings.uppercase.UNICODES) : allowed;
-        allowed = Settings.lowercase.allowed ? allowed.concat(Settings.lowercase.UNICODES) : allowed;
-        allowed = Settings.digits.allowed ? allowed.concat(Settings.digits.UNICODES) : allowed;
-
-        // if no checkboxes were checked, then we have no unicode values to choose from
-        if (allowed.length <= 0) {
-            let errorMsg = "Must enable at least one type of character.";
-            this.output.innerHTML = errorMsg;
-            throw new Error(errorMsg);
-        }
-        return allowed;
-    }
-
-    generatePassword() {
-        this.output.innerHTML = '';
-        this.updateSettings();
-        
-        // get our array of valid unicode values that we can choose from
-        const allowedChars = this.getAllowedChars();
-
-        // create an array of cryptographically-random values
-        const random = new Uint8Array(Settings.password_length);
-        window.crypto.getRandomValues(random);
-
-        // now we create the password string
-        let password = '';
-        for (let idx = 0; idx < Settings.password_length; idx++) {
-            // use the next random value to choose a unicode character
-            const validIdx = random[idx] % allowedChars.length
-            password += String.fromCharCode(allowedChars[validIdx]);
-        }
-        this.output.innerHTML += `${password}<br><br>`;
-
-        Entropy.calculateEntropy(this.output);
-    }
-
-
+    Entropy.calculateEntropy(this.output);
+  }
 }
